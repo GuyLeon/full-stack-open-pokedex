1a3473545515c94a2b7978f1a369fc8182bb1288
Félix Tamagny <Leon_felix@hotmail.fr>
2024-05-08 22:33:33 +0200

    New e2e test for pokemon page

--- e2e-tests/front_page.cy.js
+++ e2e-tests/front_page.cy.js
@@ -6,6 +6,13 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+  it('pokemon page can be navigated to', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains(
+      'chlorophyll'
+    )
+  })
 })
 
 
