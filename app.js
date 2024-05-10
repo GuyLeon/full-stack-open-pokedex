0fbc721c12edd5cfaa23486c0ad1e6dd85721c53
Félix Tamagny <Leon_felix@hotmail.fr>
2024-05-10 18:00:25 +0200

    Simulating broken deployment

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
-
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
+
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
